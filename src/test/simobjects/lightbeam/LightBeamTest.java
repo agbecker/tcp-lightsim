c7f9106536bc9c0861d441dc4d08f1242c13fc30
vlcanesin <vlcanesin@gmail.com>
2023-12-19 16:13:25 -0300

    Implementação de testes unitários

--- src/test/simobjects/lightbeam/LightBeamTest.java
+++ src/test/simobjects/lightbeam/LightBeamTest.java
@@ -2,7 +2,8 @@ package test.simobjects.lightbeam;
 
 import static core.utils.Geometry.getAngle;
 
-import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 import com.raylib.java.Raylib;
 import com.raylib.java.core.Color;
@@ -15,6 +16,25 @@ public class LightBeamTest {
     
     private static SimulationScreen simulationScreen = new SimulationScreen(600, 400, 100, 100, null);
     private static Raylib rlj = new Raylib(800, 600, "LightBeamTest");
+
+    @Test
+    public void findImagePointTest() {
+        LightBeam beam1 = new LightBeam(new Vector2(200, 100), simulationScreen, rlj); 
+        beam1.addSegment(new Vector2(400, 100));
+        beam1.addSegment(getAngle(new Vector2(400, 100), new Vector2(600, 300)));
+
+        LightBeam beam2 = new LightBeam(new Vector2(200, 100), simulationScreen, rlj); 
+        beam2.addSegment(getAngle(new Vector2(200, 100), new Vector2(400, 150)));
+
+        LightBeam beam3 = new LightBeam(new Vector2(300, 50), simulationScreen, rlj);
+        beam3.addSegment(270);
+
+        assertEquals(466.667, LightBeam.findImagePoint(beam1, beam2).x, 0.01);
+        assertEquals(166.667, LightBeam.findImagePoint(beam1, beam2).y, 0.01);
+        assertEquals(null, LightBeam.findImagePoint(beam1, beam3));
+        assertEquals(300, LightBeam.findImagePoint(beam2, beam3).x, 0.01);
+        assertEquals(125, LightBeam.findImagePoint(beam2, beam3).y, 0.01);
+    }
 
     public static void main(String[] args) {
         
