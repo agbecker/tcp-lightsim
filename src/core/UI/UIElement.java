72eae1ab975084e4fba92278f94ec1ce3c96338b
vlcanesin <vlcanesin@gmail.com>
2023-12-21 21:54:07 -0300

    Adição de constantes em UIElement e definição das dimensões das telas

--- src/core/UI/UIElement.java
+++ src/core/UI/UIElement.java
@@ -4,17 +4,15 @@ import com.raylib.java.Raylib;
 import com.raylib.java.core.Color;
 
 public interface UIElement {
-    public final int SIMULATION_SCREEN_WIDTH = 800;
-    public final int SIMULATION_SCREEN_HEIGHT = 600;
-
     public final Color LIGHT_PURPLE = new Color(102,44,246,255);
     public final Color DARK_PURPLE = new Color(61,7,194,255);
     public final Color TRACK_BLUE = new Color(0,116,203,255);
+    public final static Color MEDIUM_BLUE = new Color(0, 146, 255, 255);
+    public final static Color DARK_BLUE = new Color(0, 116, 203, 255);
+    public final static Color LIGHT_BLUE = new Color(128, 200, 255, 255);
+    public final static Color BG_BLUE = new Color(14, 137, 230, 255);
     public final Color WHITE = Color.RAYWHITE;
-    public final Color LIGHT_BLUE = new Color(0,146,255,255);
-    public final Color PALE_BLUE = new Color(128,200,255,255);
-    public final Color DARK_BLUE = new Color(0,116,203,255);
 
-    public final Raylib rlj = new Raylib(800, 600, "LightSim");
+    public final Raylib rlj = new Raylib(1200, 700, "LightSim");
     public abstract void render();
 }
--- src/core/simscreens/descriptors/SimulationScreen.java
+++ src/core/simscreens/descriptors/SimulationScreen.java
@@ -8,12 +8,22 @@ import core.simscreens.Screen;
 
 public class SimulationScreen extends Screen implements UIElement {
     
-    private final int AXIS_HEIGHT = super.getHeight()/2;
+    private static final int BEGX_DEF = 20;
+    private static final int BEGY_DEF = 20;
+    private static final int WIDTH_DEF = 750;
+    private static final int HEIGHT_DEF = 450;
+
+    private int axisHeight;
     ArrayList<ObjectToRender> objectsToRender;
+
+    public SimulationScreen(ArrayList<ObjectToRender> objectToRenders) {
+        this(WIDTH_DEF, HEIGHT_DEF, BEGX_DEF, BEGY_DEF, objectToRenders);
+    }
 
     public SimulationScreen(int width, int height, int begX, int begY, ArrayList<ObjectToRender> objectToRenders) {
         super(width, height, begX, begY);
         this.objectsToRender = objectToRenders;
+        axisHeight = super.getHeight()/2;
     }
 
     public void renderObjects() {
