3ce5c54edfa0d210bf6464d0001c926ebcb9c192
Zarauto <alvarogbecker@gmail.com>
2023-12-22 08:29:10 -0300

    Adapta Button à nova organização das classes

--- src/core/UI/UIElement.java
+++ src/core/UI/UIElement.java
@@ -6,7 +6,6 @@ import com.raylib.java.core.Color;
 public interface UIElement {
     public final Color LIGHT_PURPLE = new Color(102,44,246,255);
     public final Color DARK_PURPLE = new Color(61,7,194,255);
-    public final Color TRACK_BLUE = new Color(0,116,203,255);
     public final static Color MEDIUM_BLUE = new Color(0, 146, 255, 255);
     public final static Color DARK_BLUE = new Color(0, 116, 203, 255);
     public final static Color LIGHT_BLUE = new Color(128, 200, 255, 255);
