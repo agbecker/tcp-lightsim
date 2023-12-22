826b43c63b4b9fec12159e782ee4f080b5f725a3
Zarauto <alvarogbecker@gmail.com>
2023-12-22 08:32:56 -0300

    Update MenuButton.java

--- src/core/UI/MenuButton.java
+++ src/core/UI/MenuButton.java
@@ -1,11 +1,11 @@
-package userInterface;
+package core.UI;
 
 import java.text.ParseException;
 
 import com.raylib.java.Raylib;
 import com.raylib.java.textures.Texture2D;
 
-public class MenuButton extends Button{
+public class MenuButton extends Button {
 
     public MenuButton(int x, int y, int width, int height, String label, Raylib rlj) {
         super(x,y,width,height,label,rlj);
@@ -16,33 +16,20 @@ public class MenuButton extends Button{
     }
 
 
-<<<<<<< Updated upstream
-=======
     /* Função de teste puramente, mudar depois*/
     @Override
     public void function() {
->>>>>>> Stashed changes
         int intLabel;
 
         try {
             intLabel = Integer.parseInt(label);
         }
 
-<<<<<<< Updated upstream
-        catch (Exception e) {
-            intLabel = 1;
-            label = Integer.toString(intLabel);
-=======
         catch (NumberFormatException e){
             label = "1";
->>>>>>> Stashed changes
             return;
         };
 
         label = Integer.toString(intLabel+1);
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
     }
 }
